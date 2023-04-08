9b059073077d71772efb411b64066c8f5168015b
braamconradie <braam.conradie@gmail.com>
2023-04-08 20:16:05 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ col1, col2, col3 = st.columns([1,2,1])
 col1.markdown(" # Welcome to my app!")
 col1.markdown(" Here is some info on the app")
 
-uploaded_phot = col2.file_uploader("upload a photo")
+uploaded_photo = col2.file_uploader("upload a photo")
 camera_photo = col2.camera_input("Take a photo")
 col3.metric(label="Temperature", value="60 deg C", delta = "3 deg C")
 progress_bar = col2.progress(0)
