c775be49ef2e3b0ad6662ab6808d0657a8ec15c6
braamconradie <braam.conradie@gmail.com>
2023-04-08 20:07:48 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import time
 
 col1, col2, col3 = st.columns([1,2,1])
                              
@@ -7,3 +8,10 @@ col1.markdown(" Here is some info on the app")
 
 uploaded_phot = col2.file_uploader("upload a photo")
 camera_photo = col2.camera_input("Take a photo")
+
+progress_bar = col2.progress(0)
+for perc_completed in range(100):
+  time.sleep(0.05)
+  progress_bar.progress(perc_completed+1)
+
+col2.success("Photo uploaded successfully")
