3ebb78592d9d79ec04cfff4cc9619fa1f9992c8a
braamconradie <braam.conradie@gmail.com>
2023-04-08 20:17:25 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ for perc_completed in range(100):
 col2.success("Photo uploaded successfully")
 
 with st.expander("Click to read more"):
-  st.write("Hello, here are more details")
+  st.write("Hello, here are more details.")
   if uploaded_photo is None:
     st.image(camera_photo)
   else:
