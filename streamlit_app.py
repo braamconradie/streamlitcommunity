438be8c7bba3503b9771db79d2c83577e846b499
braamconradie <braam.conradie@gmail.com>
2023-04-08 20:02:32 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ col1, col2, col3 = st.columns([1,2,1])
                              
 col1.markdown(" # Welcome to my app!")
 col1.markdown(" Here is some info on the app")
+
+uploaded_phot = col2.file_uploader("upload a photo")
